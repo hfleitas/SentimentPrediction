14063e7f86733ea65a94c31438f4203d8d961289
hiramfleitas <hiramfleitas@hotmail.com>
2018-05-09 11:36:06 -0400

    Fix added.

--- GetSentimentExample/GetSentimentExample.py
+++ GetSentimentExample/GetSentimentExample.py
@@ -5,8 +5,8 @@ from microsoftml import rx_featurize, get_sentiment
 
 # Create the data
 text_to_analyze = p.DataFrame(data=dict(Text=[
-"How many times do I have try this?.",
-"Bottom line is that it works on ML but not on SQL."]))
+"These are not a normal stress reliever. First of all, they got sticky, hairy and dirty on the first day I received them. Second, they arrived with tiny wrinkles in their bodies and they were cold. Third, their paint started coming off. Fourth when they finally warmed up they started to stick together. Last, I thought they would be foam but, they are a sticky rubber. If these were not rubber, this review would not be so bad.",
+"These are the cutest things ever!! Super fun to play with and the best part is that it lasts for a really long time. So far these have been thrown all over the place with so many of my friends asking to borrow them because they are so fun to play with. Super soft and squishy just the perfect toy for all ages."]))
 
 # Get the sentiment scores
 sentiment_scores = rx_featurize(
